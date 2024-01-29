1739ef904e8ab9cee28c4a591963b49fe8a541c2
Vani <vani.pushpala@improve10x.com>
2024-01-29 12:02:27 +0530

    I took new class i,e QuestionService and I created controller, service package.

--- src/main/java/com/example/Quizapp/controller/QuestionController.java
+++ src/main/java/com/example/Quizapp/controller/QuestionController.java
@@ -1,4 +1,4 @@
-package com.example.Quizapp;
+package com.example.Quizapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
