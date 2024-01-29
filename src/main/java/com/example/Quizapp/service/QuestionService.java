3c86323de36103bc3ff276657cee694ce8be1925
Vani <vani.pushpala@improve10x.com>
2024-01-29 12:43:39 +0530

    I created QuestionDao object in QuestionService class

--- src/main/java/com/example/Quizapp/service/QuestionService.java
+++ src/main/java/com/example/Quizapp/service/QuestionService.java
@@ -1,12 +1,17 @@
 package com.example.Quizapp.service;
 
 import com.example.Quizapp.Question;
+import com.example.Quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
 
     public List<Question> getAllQuestions() {
 
