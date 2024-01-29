251e23b0b85db64ec9711da35df73edb1429506f
Vani <vani.pushpala@improve10x.com>
2024-01-29 12:08:37 +0530

    I created Question service object in controller class

--- src/main/java/com/example/Quizapp/controller/QuestionController.java
+++ src/main/java/com/example/Quizapp/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package com.example.Quizapp.controller;
 
+import com.example.Quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
     @GetMapping("allQuestions")
     public String getAllQuestions(){
         return "Hi, these are your questions";
