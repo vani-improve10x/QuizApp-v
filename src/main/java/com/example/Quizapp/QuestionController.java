1662eec21cbb5bb98baaaee5f58493e6cf159e69
Vani <vani.pushpala@improve10x.com>
2024-01-29 11:29:02 +0530

    I created question controller and I took Get mapping

---
+++ src/main/java/com/example/Quizapp/QuestionController.java
@@ -0,0 +1,13 @@
+package com.example.Quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuestionController {
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "Hi, these are your questions";
+    }
+}
