779f2ebd9185a8bde8cb81ba3839c497a8e384dc
Vani <vani.pushpala@improve10x.com>
2024-01-30 10:54:37 +0530

    I wrote Question controller in @postmapping

--- src/main/java/com/example/Quizapp/controller/QuestionController.java
+++ src/main/java/com/example/Quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.example.Quizapp.controller;
 import com.example.Quizapp.Question;
 import com.example.Quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,10 @@ public class QuestionController {
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable("category") String category){
         return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
     }
 }
