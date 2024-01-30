201f7b2be56eb3784da4912d6fc28fff0b7cc1ea
Vani <vani.pushpala@improve10x.com>
2024-01-30 10:58:29 +0530

    I wrote Question service in business logic

--- src/main/java/com/example/Quizapp/service/QuestionService.java
+++ src/main/java/com/example/Quizapp/service/QuestionService.java
@@ -20,4 +20,9 @@ public class QuestionService {
     public List<Question> getQuestionsByCategory(String category) {
         return questionDao.findByCategory(category);
     }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "success";
+    }
 }
