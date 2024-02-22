5856dd03467db05ea4ccda799a0c937e081aee5e
Vani <vani.pushpala@improve10x.com>
2024-02-22 13:34:27 +0530

    I added QuestionDao in one method and I wrote Query

--- src/main/java/com/example/Quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/Quizapp/dao/QuestionDao.java
@@ -2,6 +2,7 @@ package com.example.Quizapp.dao;
 
 import com.example.Quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,6 @@ import java.util.List;
 public interface QuestionDao extends JpaRepository<Question,Integer> {
 
     public List<Question>findByCategory(String category);
-
+    @Query(value = "select * from question q where q.category=:category order by random() limit : numQ", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
