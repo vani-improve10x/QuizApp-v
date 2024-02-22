e556698c176c17881277a22049ac00e304d168da
Vani <vani.pushpala@improve10x.com>
2024-02-22 12:56:04 +0530

    I created quiz service in quiz dao object

--- src/main/java/com/example/Quizapp/service/QuizService.java
+++ src/main/java/com/example/Quizapp/service/QuizService.java
@@ -1,10 +1,14 @@
 package com.example.Quizapp.service;
 
+import com.example.Quizapp.dao.QuizDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuizService {
+
+    @Autowired
+    QuizDao quizDao;
 
 
 }
