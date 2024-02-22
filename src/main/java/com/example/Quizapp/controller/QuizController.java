7eca9253f71bb86949451d624ad3080ef7c2007a
Vani <vani.pushpala@improve10x.com>
2024-02-22 12:38:04 +0530

    I took quiz service class and quiz controller in added for quiz service object.

--- src/main/java/com/example/Quizapp/controller/QuizController.java
+++ src/main/java/com/example/Quizapp/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.example.Quizapp.controller;
 
+import com.example.Quizapp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
+
+    @Autowired
+    QuizService quizService;
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return new ResponseEntity<>("Im here", HttpStatus.OK);
     }
+
 
 }
---
+++ src/main/java/com/example/Quizapp/service/QuizService.java
@@ -0,0 +1,8 @@
+package com.example.Quizapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+
+}
