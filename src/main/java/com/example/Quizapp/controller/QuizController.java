20b1948b6bc56a5a628c4032ff021c55ae15a2fe
Vani <vani.pushpala@improve10x.com>
2024-02-23 16:22:53 +0530

    I wrote get mapping api

--- src/main/java/com/example/Quizapp/controller/QuizController.java
+++ src/main/java/com/example/Quizapp/controller/QuizController.java
@@ -1,13 +1,13 @@
 package com.example.Quizapp.controller;
 
+import com.example.Quizapp.model.Question;
 import com.example.Quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -18,5 +18,10 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("get/{id}")
+
+    public ResponseEntity<List<Question>> getQuizQuestions(@PathVariable("id") Integer id){
+        
     }
 }
